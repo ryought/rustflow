b81601aed22d55451f5caff9feb344e4a2f785b3
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2022-01-24 17:35:30 +0900

    add cost trait

--- src/min_flow/convex.rs
+++ src/min_flow/convex.rs
@@ -1,6 +1,7 @@
 //!
 //! Flow network definitions for convex cost.
 //!
+use super::flow::EdgeCost;
 use petgraph::graph::{DiGraph, EdgeIndex};
 use std::collections::HashMap;
 
@@ -15,16 +16,22 @@ pub struct ConvexFlowEdge {
     pub capacity: u32,
     /// cost function
     /// it is a convex function of the current flow
-    pub cost: fn(u32) -> f64,
+    pub convex_cost: fn(u32) -> f64,
 }
 
 impl ConvexFlowEdge {
-    pub fn new(demand: u32, capacity: u32, cost: fn(u32) -> f64) -> ConvexFlowEdge {
+    pub fn new(demand: u32, capacity: u32, convex_cost: fn(u32) -> f64) -> ConvexFlowEdge {
         ConvexFlowEdge {
             demand,
             capacity,
-            cost,
+            convex_cost,
         }
+    }
+}
+
+impl EdgeCost for ConvexFlowEdge {
+    fn cost(&self, flow: u32) -> f64 {
+        (self.convex_cost)(flow)
     }
 }
 
@@ -40,3 +47,21 @@ pub type ConvexFlowGraph = DiGraph<(), ConvexFlowEdge>;
 /// create an parallel edges
 ///
 pub fn to_flow_graph(graph: &ConvexFlowGraph) {}
+
+//
+// tests
+//
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn convex_flow_edge_new() {
+        let e = ConvexFlowEdge::new(1, 5, |f| 10.0 * f as f64);
+        assert_eq!(50.0, (e.convex_cost)(5));
+        assert_eq!(50.0, e.cost(5));
+        assert_eq!(1, e.demand);
+        assert_eq!(5, e.capacity);
+    }
+}
